92d0f87c4f766cd5ec0393c90afda59d9bbf157c
binitworld <contactbinitbhushan@gmail.com>
2023-09-15 17:27:23 +0530

    15: 05

---
+++ learn1.java
@@ -0,0 +1,25 @@
+public class learn1 {
+    public static void main(String[ ]args){
+        System.out.println("Hi guys this is Binit!!");
+    }
+}
+
+// Practice set 1
+
+public class learn1{
+    public static void main(String[] args){
+        System.out.println("Hello ! ,World");
+    }
+}
+
+// Practice set 2:Write a Java program to print the current date and time.
+
+public class learn1{
+    public static void main(String[] args){
+        
+        System.out.println("Todays is Date is 15th september and");
+        System.out.println("Current time is about 5:25 pm");
+
+    }
+
+}
