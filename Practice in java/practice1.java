ae6ebc209d9e39698256c14bec5159d1b890f1b6
binitworld <contactbinitbhushan@gmail.com>
2023-10-05 20:20:04 +0530

    05-10---4

---
+++ Practice in java/practice1.java
@@ -0,0 +1,10 @@
+public class practice1{
+    public static void main(String[] args) {
+        float a = 44;
+        float b = 56;
+        float result = a + b;
+        System.out.println("Sum of a and b = " + result);
+        // System.out.println("Type of b: " + b.getClass().getSimpleName());
+        // System.out.println("Type of a: " + a.getClass().getSimpleName());
+    }
+}
