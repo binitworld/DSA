e8009e1ba9707062c5adbf90f0be6556ee3db054
binitworld <contactbinitbhushan@gmail.com>
2023-10-06 04:30:07 +0530

    06-10---7

---
+++ Practice in java/practice4.java
@@ -0,0 +1,17 @@
+public class practice4 {
+    public static void main(String [] arg)
+    {
+        char name ='n';
+        char nam ='e';
+        char nati ='t';
+
+        char rest = name;
+        char res = nam;
+        char re = nati;
+        
+        System.out.println(rest);
+        System.out.println(res);
+        System.out.println(re);
+
+    }
+}
