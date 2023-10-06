161062959d0542bd1f371516b729165e43314c05
binitworld <contactbinitbhushan@gmail.com>
2023-10-06 06:16:31 +0530

    study

---
+++ Dsa-on-java/casting.java
@@ -0,0 +1,10 @@
+public class casting {
+    public static void main(String[] args){
+
+        int a = 200;  //here modulus operation is is perform since in this type casting the byte is all about --127  if we increases the value this use the modulus operation; 
+        byte k =(byte) a;
+
+        System.out.println(k);
+
+    }
+}
---
+++ Dsa-on-java/type.java
@@ -0,0 +1,13 @@
+public class type {
+    public static void main(String[] args){
+
+        byte m =56;
+
+        int  a = m;
+
+        
+
+        System.out.println(a);
+    }
+    
+}
