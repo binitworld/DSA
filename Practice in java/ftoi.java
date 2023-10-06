8682c26f69ff04bc087c77d90703ff54a23f739f
binitworld <contactbinitbhushan@gmail.com>
2023-10-06 14:42:52 +0530

    Type Casting Practice

---
+++ Practice in java/ftoi.java
@@ -0,0 +1,13 @@
+public class ftoi {
+    public static void main(String[] args){
+        // Write a program to convert a float to an integer.
+
+        float j = 78f;
+        System.out.println("The float value is :"+j);
+        int a =(int) j;
+        
+        System.out.println("The type conversion/casting from float to int is :"+a);
+
+
+    }
+}
---
+++ Practice in java/itof.java
@@ -0,0 +1,15 @@
+public class itof {
+    public static void main(String[] args){
+
+        // Write a program to convert an integer to a float.
+
+        int m = 23;
+        System.out.println("The value of integer is :"+m);
+
+        float n = (float) m;
+
+        System.out.println("The converted value from integer to float is :"+n);
+        
+
+    }
+}
