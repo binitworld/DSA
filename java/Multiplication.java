4b2842bfb8dcd23704fb9ac7f535771b785a758e
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-25 00:20:59 +0530

    new commit

---
+++ java/Multiplication.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in) ;
+
+        System.out.print("Enter The first number : ");
+        int a = input.nextInt();
+        
+        System.out.print("Enter the Second number:");
+        int b =  input.nextInt();
+
+        int c = a*b;
+        System.out.println("The multiplication of two number is : " + c );
+    }
+}
