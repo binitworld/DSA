ceb5ae73e0f231649766e900fa8195b94a4c0263
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-25 00:20:11 +0530

    new commit

---
+++ java/Substraction.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Substraction{
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the First number: ");
+        int a = input.nextInt();
+        
+        System.out.print("Enter the Second number: ");
+        int b = input.nextInt();
+
+        int c = a-b;
+        System.out.println("The Submission of two number is : " + c);
+    }
+}
