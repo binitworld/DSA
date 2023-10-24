fb286fd8ef61b2a170f8b39b818e24a629485332
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-25 00:19:53 +0530

    new commit

---
+++ java/Unicode.java
@@ -0,0 +1,16 @@
+// import java.util.Scanner;
+
+public class Unicode {
+    public static void main(String[] args) {
+        // Scanner input = new Scanner(System.in);
+
+        System.out.println("नमस्ते");
+        System.out.println("你好");
+        System.out.println("வணக்கம்");
+        System.out.println("హలో");
+
+
+
+
+    }
+}
