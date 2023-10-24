0a007b33f55bbbde45aaf2ef2919bb90539e6917
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-25 00:20:21 +0530

    new commit

---
+++ java/Primitives.java
@@ -0,0 +1,25 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int roll_no = 34;
+        System.out.println(roll_no);
+        
+        //string is not a primitives
+        
+        char letter='b';
+        System.out.println(letter);
+        
+        float marks=99.67f;  // To getting the correct value we have to use f over there to avoid over double
+        System.out.println(marks);
+        
+        double large_decimal_number =111123445776.563565168;
+        System.out.println(large_decimal_number);
+
+        long  largeInteger =876757473L; // We have to implement to large number conversion
+        System.out.println(largeInteger);
+        
+        boolean check = true;
+        System.out.println(check);
+    }
+
+
+}
