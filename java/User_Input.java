794e46b5c96364a3663f1ee54388243f4238bd41
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-25 00:19:36 +0530

    new commit

---
+++ java/User_Input.java
@@ -0,0 +1,11 @@
+public class User_Input {
+    
+    public static void main (String[] args){ 
+        System.out.println("This is experience  test");
+        if (args.length>1){
+            System.out.println(args[1]);
+        }
+
+
+    }
+}
