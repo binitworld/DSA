b8978157e7af7e8524ec2039c29dbe19f24227ec
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-25 00:21:46 +0530

    new commit

---
+++ java/Testc.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Testc {
+
+    public static void main(String[] args){
+
+        Scanner input =new Scanner(System.in);
+        System.out.print("Enter your Roll number:  ");
+        int roll_number = input.nextInt();
+        System.out.println("YOUR  Roll number is : " + roll_number);
+
+    }
+}
