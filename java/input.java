fd5e2554b5fba8087fda143943a8d8e8b0512814
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-10-25 00:20:39 +0530

    new commit

---
+++ java/input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class input{
+    public static void main(String[] args) {
+        // System.out.println("Hey! How are you ");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println(input.nextInt());
+    }
+}
