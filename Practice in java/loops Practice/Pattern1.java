da4d5a0bf3f4ead57ed1cf9e2358963100150866
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-11-10 04:00:22 +0530

    0

--- Practice in java/loops Practice/Pattern1.java
+++ Practice in java/loops Practice/Pattern1.java
@@ -2,26 +2,7 @@ import java.util.Scanner;
 
 public class Pattern1 {
     public static void main(String[] args) {
-        
 
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter the size of the Square :");
-        int size = input.nextInt();
-
-        for (int i=0 ; i <= size ; i++ ){
-            for(int j = i; j < size; j++){
-                System.out.print("90");
-
-            }
-            System.out.println();
-        }
-    }
-}
-/* import java.util.Scanner;
-
-public class Pattern1 {
-    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the size of the Square :");
@@ -29,10 +10,29 @@ public class Pattern1 {
 
         for (int i = 0; i <= size; i++) {
             for (int j = i; j < size; j++) {
-                System.out.print("#");
+                System.out.print("*");
+
             }
             System.out.println();
         }
     }
 }
-*/
+/*
+ * import java.util.Scanner;
+ * 
+ * public class Pattern1 {
+ * public static void main(String[] args) {
+ * Scanner input = new Scanner(System.in);
+ * 
+ * System.out.print("Enter the size of the Square :");
+ * int size = input.nextInt();
+ * 
+ * for (int i = 0; i <= size; i++) {
+ * for (int j = i; j < size; j++) {
+ * System.out.print("#");
+ * }
+ * System.out.println();
+ * }
+ * }
+ * }
+ */
