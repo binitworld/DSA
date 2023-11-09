e85eb4c0786298be4aaa75f85028be83e10cde05
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-11-10 03:57:51 +0530

    10-11- 1

---
+++ Dsa-on-java/Arrays/ArrFirst.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ArrFirst {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // store a person's name
+
+        System.out.print("Enter the name of the peron :");
+        String name = input.nextLine();
+        System.out.println("The name of the person is " + name);
+
+        // Store a roll num
+
+        System.out.print("Enter the Roll num to stored here : ");
+        int roll_num = input.nextInt();
+        System.out.println("And his roll number is " + roll_num)
+
+    
+
+    }
+}
