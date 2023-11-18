012f38640e27ded13acbec20b14b8001ead18275
Binit Bhushan <contactbinitbhushan@gmail.com>
2023-11-19 02:06:52 +0530

    .

---
+++ Dsa-on-java/Arrays/Operations/Maximum/Minimum2D.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Minimum2D {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[][] net = new int[3][3];
+        for (int i = 0; i < net.length; i++) {
+            for (int j = 0; j < net(i).length; j++) {
+                System.out.print("Enter the data for row " + (i + 1) + ",& its column is" + (j + 1));
+                net[i][j] = nextInt();
+
+            }
+        }
+    }
+}
