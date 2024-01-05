3b30e3f591a9942e10cb8698624b74d435f51752
Binit Bhushan <contactbinitbhushan@gmail.com>
2024-01-06 02:51:35 +0530

    06-01-24

---
+++ c++/Basics/base.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello WORLD!"<<endl;
+    cout<<"Namaste duniya !";
+    cout<<"Nah duniya !" << '\n' ;
+    cout<<"Na duniya ";
+    cout<<'\n';
+
+}
