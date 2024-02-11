20c177f586c4165bc318d85b6df441d492874470
Binit Bhushan <contactbinitbhushan@gmail.com>
2024-02-11 20:04:56 +0530

    looop

---
+++ c++/Basics/forloop.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i ;
+    for (size_t i = 1; i < 11; i++)
+    {
+        cout<<"Hello Binit"<< i<<endl;
+
+    }
+    cout<<i<<endl;
+    
+    return 0;
+}
