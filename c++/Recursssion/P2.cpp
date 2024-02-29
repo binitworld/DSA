762c6e9a9c88f7dd25e3616961c0c5efe31beeaf
Binit Bhushan <contactbinitbhushan@gmail.com>
2024-02-29 20:04:00 +0530

    practice 2

---
+++ c++/Recursssion/P2.cpp
@@ -0,0 +1,16 @@
+// Print Name N times :)
+#include <bits/stdc++.h>
+using namespace std;
+    void f(int i,int n){
+        
+        if (i>=n) return;
+        cout<<" Binit " << endl;
+        f(i+1,n);
+    }
+int main(){
+    int n;
+    cout<<"Enter the  Integer number to print The name upto : ";
+    cin>>n;
+    f(0,n);
+    return 0;
+}
