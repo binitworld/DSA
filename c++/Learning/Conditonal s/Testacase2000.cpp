7f3713cebceb4ae06abe7c16c098d38d9a14395e
Binit Bhushan <contactbinitbhushan@gmail.com>
2024-03-13 22:57:35 +0530

    Trying

---
+++ c++/Learning/Conditonal s/Testacase2000.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int a;
+    int b;
+    cout <<"Enter the First  number :";
+    cin>>a;
+    cout <<"Enter the Second number :";
+    cin>>b;
+
+    if (a>b){
+        cout <<"The first number is grater than the second number" <<endl;
+    }else{
+        cout<<"The second number is grater than the first number "<<endl;
+    }
+
+    return 0;
+}
