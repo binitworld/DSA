c8519e82fdf91e153f73b6ebc4a22e9a7af913ef
Binit Bhushan <contactbinitbhushan@gmail.com>
2024-03-17 02:03:55 +0530

    sum of n number using  while loop

---
+++ c++/Learning/Loop/While LOOP/SumOfNTerm.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<< " : ";
+    cin>>n;
+
+    int i =1;
+    int sum = 0;
+    
+
+    while (i <= n){
+        sum = sum + i;
+        i=i+1;
+        }
+        cout <<"value of sum = " <<sum <<endl;
+}
