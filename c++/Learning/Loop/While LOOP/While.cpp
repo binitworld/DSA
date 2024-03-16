d2d0a82bc0a5f4e426e8fd08c9dd1da541c87877
Binit Bhushan <contactbinitbhushan@gmail.com>
2024-03-17 02:05:34 +0530

    Using while loop print upto nth term`

---
+++ c++/Learning/Loop/While LOOP/While.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num;
+    cout <<"Enter the number :";
+    cin>>num;
+    int i = 1;
+
+
+    while ( i <= num){
+        cout<<i<<endl;
+        i = i+1;//Here we are moving i+1 its value .. till num
+    }
+    return 0;
+}
