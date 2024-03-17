ce4cef24d77162d208b773765916660c7f7096c7
Binit Bhushan <contactbinitbhushan@gmail.com>
2024-03-17 17:22:54 +0530

    Performing And operation

---
+++ c++/Learning/Bit Wise Operator/AndOperation.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout <<"Enter the First number : ";
+    cin >>a;
+    cout<<"Enter the second number : ";
+    cin>>b;
+
+    int c = a & b;
+    cout <<"the resulted output will be : "<< c <<endl;
+}
