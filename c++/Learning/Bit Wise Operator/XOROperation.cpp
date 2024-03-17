7b85a4ec86daf04ef1765265edfec6c0b1886853
Binit Bhushan <contactbinitbhushan@gmail.com>
2024-03-17 17:37:57 +0530

    XOR OPERATION

---
+++ c++/Learning/Bit Wise Operator/XOROperation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+    int a ;
+    int b;
+    cout <<"Enter the First number :";
+    cin>>a;
+    cout <<"Enter the Second number :";
+    cin>>b;
+
+
+    int c= a^b;
+    cout << "The Resulted output is :"<<c<<endl;
+}
