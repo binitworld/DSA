7da5428119d1daff5f1fb7051bf691401128e005
Binit Bhushan <contactbinitbhushan@gmail.com>
2024-03-17 17:27:40 +0530

    Or Operation

---
+++ c++/Learning/Bit Wise Operator/OrOperation.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    cout<<"Enter the First Number : ";
+    cin>>a;
+    cout<<"Enter the second Number :";
+    cin >>b;
+
+    int c = a|b;
+    cout << "The results will be : "<< c<< endl;
+}
