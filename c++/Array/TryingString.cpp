f39cc701224e76f1f2538d4bae382f12e0402d85
Binit Bhushan <contactbinitbhushan@gmail.com>
2024-03-26 19:10:50 +0530

    Storing a string type  in an array

---
+++ c++/Array/TryingString.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    string arr[3]={"Ram","Shiv","Durga"};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    for (int i = 0; i<n;i++){
+        cout << arr[i]<< endl;
+    }
+}
