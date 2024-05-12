c4fbecbf30db805996889da16e19dcec9dfb227d
Binit Bhushan <contactbinitbhushan@gmail.com>
2024-05-13 01:51:48 +0530

    And Operator

---
+++ c++/Basics/AndOpperations.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the first no. : ";
+    cin>>a;
+    cout<<"Enter the second no. : ";
+    cin>>b;
+    int c= a & b;
+    cout<<" the and operation is :  "<< c << endl;
+    return 0;
+}
